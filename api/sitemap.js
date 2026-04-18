feb11946eb1bac070044f62494d2b83fc9419245
Abrar <smfahimabrar93@gmail.com>
2026-04-18 14:48:36 -0400

    Add 20 destination pages to dynamic sitemap

--- api/sitemap.js
+++ api/sitemap.js
@@ -21,23 +21,33 @@ export default async function handler(req, res) {
     <priority>0.6</priority>
   </url>`).join('');
 
+  const destinations = ['tokyo','paris','bali','dubai','new-york','london','rome','bangkok','singapore','maldives','barcelona','istanbul','seoul','kyoto','amsterdam','prague','santorini','lisbon','cape-town','sydney'];
+  const destUrls = destinations.map(d => `
+  <url>
+    <loc>https://getatlas.ca/plan/${d}</loc>
+    <lastmod>2026-04-18</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.8</priority>
+  </url>`).join('');
+
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
     <loc>https://getatlas.ca/</loc>
-    <lastmod>2026-04-13</lastmod>
-    <changefreq>weekly</changefreq>
+    <lastmod>2026-04-18</lastmod>
+    <changefreq>daily</changefreq>
     <priority>1.0</priority>
   </url>
+  ${destUrls}
   <url>
     <loc>https://getatlas.ca/privacy</loc>
-    <lastmod>2026-04-13</lastmod>
+    <lastmod>2026-04-18</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.3</priority>
   </url>
   <url>
     <loc>https://getatlas.ca/terms</loc>
-    <lastmod>2026-04-13</lastmod>
+    <lastmod>2026-04-18</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.3</priority>
   </url>
